38b60e5c18331b58169f6f916c04ae58ccb02cf5
Welt <tienduy0123@gmail.com>
2025-05-22 20:54:07 +0200

    Init commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,80 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Shuu ni Ichido Classmate wo Kau Hanashi ~ Futari no Jikan, Iiwake no Gosen\'en ~',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://w3lt.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/shuu-ni-ichido-classmate-wo-kau-hanashi/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'W3lt', // Usually your GitHub org/user name.
+  projectName: 'Shuu ni Ichido Classmate wo Kau Hanashi', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          routeBasePath: '/wn',
+          // Please change this to your repo.
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    navbar: {
+      title: 'Shuu ni Ichido Classmate wo Kau Hanashi',
+      logo: {
+        alt: '',
+        src: 'img/logo.png',
+        href: '/wn/chapter-111',
+      },
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        redirects: [
+          {
+            from: '/',
+            to: '/wn/chapter-111',
+          },
+        ],
+      },
+    ],
+  ],
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10vh' }}>
+        <img
+          src="/shuu-ni-ichido-classmate-wo-kau-hanashi/img/logo.png"
+          alt="Shuu ni Ichido Classmate wo Kau Hanashi Logo"
+          style={{ width: 200, marginBottom: 32 }}
+        />
+        <h1>Welcome to Shuu ni Ichido Classmate wo Kau Hanashi</h1>
+        <Link
+          className="button button--primary button--lg"
+          to="/wn/chapter-111"
+          style={{ marginTop: 32 }}
+        >
+          Read Web Novel
+        </Link>
+      </main>
+    </Layout>
+  );
+}
